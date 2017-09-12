ac401a2de1cd3ffd6143d4615f7ca000d51aba3c
Cesar <Cesar@DESKTOP-FICQ4B6>
2017-09-12 16:23:23 -0500

    Versión alicante septiembre 12

---
+++ src/java/beans/SelecCursosBean.java
@@ -0,0 +1,23 @@
+package beans;
+public class SelecCursosBean {
+    private String email;
+    private String[] cursosId;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String[] getCursosId() {
+        return cursosId;
+    }
+
+    public void setCursosId(String[] cursosId) {
+        this.cursosId = cursosId;
+    }
+    
+    
+}
